1eecd57f4626be089ca992242fb055bc33ff7ad0
Jonathan <dos.santos.2k23@gmail.com>
2025-07-28 20:25:30 -0300

    feat: implement Command0 class for zero-parameter commands

--- lib/core/commands/commands.dart
+++ lib/core/commands/commands.dart
@@ -39,3 +39,12 @@ abstract class Command<Output extends Object> extends ChangeNotifier {
     }
   }
 }
+
+class Command0<Output extends Object> extends Command<Output> {
+  final CommandAction0<Output> action;
+  Command0(this.action);
+
+  Future<void> execute() async {
+    await _execute(() => action());
+  }
+}
