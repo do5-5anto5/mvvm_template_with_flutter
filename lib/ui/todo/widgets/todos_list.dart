d54586238dc5148949e33b219c4a09c4ccf34be8
Jonathan <dos.santos.2k23@gmail.com>
2025-07-30 21:59:48 -0300

    feat: receive todoViewmodel.delete(todo) callback in TodosList and TodoTile widgets

--- lib/ui/todo/widgets/todos_list.dart
+++ lib/ui/todo/widgets/todos_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_template_with_flutter/core/typedefs/delete_todo.dart';
 import 'package:mvvm_template_with_flutter/domain/models/todo.dart';
 import 'package:mvvm_template_with_flutter/ui/todo/widgets/todo_tile.dart';
 
 class TodosList extends StatelessWidget {
-  const TodosList({super.key, required this.todos});
+  const TodosList({super.key, required this.todos, required this.onDeleteTodo});
   final List<Todo> todos;
+  final OnDeleteTodo onDeleteTodo;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +16,7 @@ class TodosList extends StatelessWidget {
     return ListView.builder(
       itemCount: todos.length,
       itemBuilder: (context, index) {
-        return TodoTile(todo: todos[index]);
+        return TodoTile(todo: todos[index], onDeleteTodo: onDeleteTodo);
       },
     );
   }
