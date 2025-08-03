f5008c34c1875f1ace9f9e490989ab1235b3f761
Jonathan <dos.santos.2k23@gmail.com>
2025-08-03 17:17:39 -0300

    feat: implement Todo.fromJson in todo.dart - this method parses json to Todo object

--- lib/domain/models/todo.dart
+++ lib/domain/models/todo.dart
@@ -3,4 +3,8 @@ class Todo {
   final String name;
 
   Todo({required this.id, required this.name});
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(id: json["id"], name: json["name"]);
+  }
 }
