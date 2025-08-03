7e8d5acfc28b4993182be3558460b6798b9483fd
Jonathan <dos.santos.2k23@gmail.com>
2025-08-03 19:50:59 -0300

    feat: implement tests from ApiClient get, post and delete

---
+++ test/data/services/api_client_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mvvm_template_with_flutter/data/services/api/api_client.dart';
+import 'package:mvvm_template_with_flutter/domain/models/todo.dart';
+import 'package:mvvm_template_with_flutter/utils/result/result.dart';
+
+void main() {
+  group('[ApiClient]', () {
+    late ApiClient apiClient;
+
+    setUp(() {
+      apiClient = ApiClient();
+    });
+    test('should return Ok when getTodos()', () async {
+      final result = await apiClient.getTodos();
+
+      expect(result.asOk.value, isA<List<Todo>>());
+    });
+
+    test('should return Ok when postTodo()', () async {
+      final Todo todoToCreate = Todo(name: 'Todo created on Test');
+      final result = await apiClient.postTodo(todoToCreate);
+
+      expect(result.asOk.value, isA<Todo>());
+      expect(result.asOk.value.id, isNotNull);
+      expect(result.asOk.value.name, 'Todo created on Test');
+    });
+
+    test('should return Ok when deleteTodo()', () async {
+      final Todo todoToCreate = Todo(name: 'Todo created on Test'); 
+           
+      final createTodoResult = await apiClient.postTodo(todoToCreate);
+
+      final result = await apiClient.delete(createTodoResult.asOk.value);
+
+      expect(result.asOk, isA<Result<void>>());
+    });
+  });
+}
