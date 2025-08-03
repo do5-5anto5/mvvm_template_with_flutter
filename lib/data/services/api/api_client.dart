7218c0625324e18709c7c32cd63387d42728fd5e
Jonathan <dos.santos.2k23@gmail.com>
2025-08-03 17:43:44 -0300

    refactor: 1. to improve parsement logic, stringData in getTodos from `Apiclient' needs to be a List<dynamic> 2. refactor api_client.dart package location

--- lib/data/services/api/api_client.dart
+++ lib/data/services/api/api_client.dart
@@ -24,7 +24,7 @@ class ApiClient {
 
       if (response.statusCode == 200) {
         final stringData = await response.transform(utf8.decoder).join();
-        final json = jsonDecode(stringData) as List<Map<String, dynamic>>;
+        final json = jsonDecode(stringData) as List<dynamic>;
         final List<Todo> todos = json.map((e) => Todo.fromJson(e)).toList();
         return Result.ok(todos);
       } else {
