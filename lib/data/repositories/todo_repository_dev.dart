29b16d7010bf82bea24e64b8106ee3c11d868c12
Jonathan <dos.santos.2k23@gmail.com>
2025-08-05 22:32:11 -0300

    feat: implement getById in Todo repositories

--- lib/data/repositories/todo_repository_dev.dart
+++ lib/data/repositories/todo_repository_dev.dart
@@ -29,4 +29,9 @@ class TodoRepositoryDev extends TodoRepository {
   Future<Result<List<Todo>>> get() async {
     return Result.ok(_todos);
   }
+
+  @override
+  Future<Result<Todo>> getById(String id) async {
+    return Result.ok(_todos.where((e) => e.id == id).first);
+  }
 }
