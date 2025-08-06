dd7846bcb146a9759d7ce9cde3239a487772889d
Jonathan <dos.santos.2k23@gmail.com>
2025-08-05 22:30:18 -0300

    feat: implement toJson() method in todo.dart

--- lib/domain/models/todo.dart
+++ lib/domain/models/todo.dart
@@ -7,4 +7,27 @@ class Todo {
   factory Todo.fromJson(Map<String, dynamic> json) {
     return Todo(id: json["id"], name: json["name"]);
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      // 'description': description,
+      // 'done': done,
+    };
+  }
+
+  Todo copyWith({
+    String? id,
+    String? name,
+    String? description,
+    bool? done,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      // description: description ?? this.description,
+      // done: done ?? this.done,
+    );
+  }
 }
