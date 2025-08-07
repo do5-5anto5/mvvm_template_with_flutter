c6561612ce384b5e693daa1e983b2fefae6d5797
Jonathan <dos.santos.2k23@gmail.com>
2025-08-07 16:31:53 -0300

    feat: pass todoViewmodel across TodoScreen widget tree, to show updateTodo() happening in UI

--- lib/ui/todo/widgets/todos_list.dart
+++ lib/ui/todo/widgets/todos_list.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_template_with_flutter/utils/typedefs/delete_todo.dart';
-import 'package:mvvm_template_with_flutter/domain/models/todo.dart';
+import 'package:mvvm_template_with_flutter/ui/todo/viewmodels/todo_viewmodel.dart';
 import 'package:mvvm_template_with_flutter/ui/todo/widgets/todo_tile.dart';
+import 'package:mvvm_template_with_flutter/utils/typedefs/delete_todo.dart';
 
 class TodosList extends StatelessWidget {
-  const TodosList({super.key, required this.todos, required this.onDeleteTodo});
-  final List<Todo> todos;
+  const TodosList({
+    super.key,
+    required this.onDeleteTodo,
+    required this.todoViewmodel,
+  });
   final OnDeleteTodo onDeleteTodo;
+  final TodoViewmodel todoViewmodel;
 
   @override
   Widget build(BuildContext context) {
-    if (todos.isEmpty) {
+    if (todoViewmodel.todos.isEmpty) {
       return const Center(child: Text('Nenhuma tarefa por enquanto'));
     }
     return ListView.builder(
-      itemCount: todos.length,
+      itemCount: todoViewmodel.todos.length,
       itemBuilder: (context, index) {
-        return TodoTile(todo: todos[index], onDeleteTodo: onDeleteTodo);
+        return TodoTile(
+          todo: todoViewmodel.todos[index],
+          onDeleteTodo: onDeleteTodo,
+          todoViewmodel: todoViewmodel,
+        );
       },
     );
   }
