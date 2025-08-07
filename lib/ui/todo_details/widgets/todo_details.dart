47f16f9d327ad4aee2fb17e9e9b8cdb6034a3193
Jonathan <dos.santos.2k23@gmail.com>
2025-08-06 22:07:20 -0300

    feat/refactor: - feat: create TodoName widget - refact: problem: predefined widget `child` parameter from ListenableBuilder was rendering before todoViewmodel.todo get filled   - solution: create and return TodoName widget directly inside the builder

--- lib/ui/todo_details/widgets/todo_details.dart
+++ lib/ui/todo_details/widgets/todo_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_template_with_flutter/ui/todo_details/viewmodels/todo_details_viewmodel.dart';
+import 'package:mvvm_template_with_flutter/ui/todo_details/widgets/todo_name.dart';
 
 class TodoDetailsScreen extends StatefulWidget {
   const TodoDetailsScreen({super.key, required this.todoDetailsViewmodel});
@@ -17,7 +18,7 @@ class _TodoDetailsScreenState extends State<TodoDetailsScreen> {
       appBar: AppBar(title: Text('Listando detalhes do Todo')),
       body: ListenableBuilder(
         listenable: widget.todoDetailsViewmodel.load,
-        builder: (context, child) {
+        builder: (context, _) {
           if (widget.todoDetailsViewmodel.load.running) {
             return const Center(child: CircularProgressIndicator());
           }
@@ -26,22 +27,11 @@ class _TodoDetailsScreenState extends State<TodoDetailsScreen> {
               child: Text('Ocorreu um erro ao carregar detalhes da tarefa'),
             );
           }
-          return child!;
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TodoName(todo: widget.todoDetailsViewmodel.todo),
+          );
         },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            padding: EdgeInsets.all(8),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-              color: Colors.blue
-            ),
-            child: Text(widget.todoDetailsViewmodel.todo.name, style: const TextStyle(
-              color: Colors.white
-            )),
-          ),
-        ),
       ),
     );
   }
