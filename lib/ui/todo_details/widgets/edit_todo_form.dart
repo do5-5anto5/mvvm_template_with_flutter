c3a6a8798d88d61d5d83dcdee3b751198a8b1046
Jonathan <dos.santos.2k23@gmail.com>
2025-08-09 08:58:38 -0300

    feat: connecting todoDetailsViewmodel across TodoDetailsScreen to EditTodoForm, so it can trigger the update todo use case. Also implement a Snack Bar showing process visual feedback to user

--- lib/ui/todo_details/widgets/edit_todo_form.dart
+++ lib/ui/todo_details/widgets/edit_todo_form.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_template_with_flutter/domain/models/todo.dart';
+import 'package:mvvm_template_with_flutter/ui/todo_details/viewmodels/todo_details_viewmodel.dart';
 
 class EditTodoForm extends StatefulWidget {
-  const EditTodoForm({super.key});
+  const EditTodoForm({
+    super.key,
+    required this.todo,
+    required this.todoDetailsViewmodel,
+  });
+
+  final Todo todo;
+  final TodoDetailsViewmodel todoDetailsViewmodel;
 
   @override
   State<EditTodoForm> createState() => _EditTodoFormState();
 }
 
 class _EditTodoFormState extends State<EditTodoForm> {
-  late final _nameController = TextEditingController();
-  late final _descriptionController = TextEditingController();
+  late final _nameController = TextEditingController(text: widget.todo.name);
+  late final _descriptionController = TextEditingController(
+    text: widget.todo.description,
+  );
 
   final _formKey = GlobalKey<FormState>();
 
   final _verticalGap = const SizedBox(height: 8);
+
+  @override
+  void initState() {
+    widget.todoDetailsViewmodel.updateTodo.addListener(_onUpdateTodo);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +40,7 @@ class _EditTodoFormState extends State<EditTodoForm> {
         child: Column(
           children: [
             const Text('Editando Tarefa'),
+
             _verticalGap,
             TextFormField(
               controller: _nameController,
@@ -37,6 +55,7 @@ class _EditTodoFormState extends State<EditTodoForm> {
                 return null;
               },
             ),
+
             _verticalGap,
             Expanded(
               child: TextFormField(
@@ -49,10 +68,18 @@ class _EditTodoFormState extends State<EditTodoForm> {
                 ),
               ),
             ),
+
             _verticalGap,
             ElevatedButton(
               onPressed: () {
-                if (_formKey.currentState?.validate() == true) {}
+                if (_formKey.currentState?.validate() == true) {
+                  widget.todoDetailsViewmodel.updateTodo.execute(
+                    widget.todo.copyWith(
+                      name: _nameController.text,
+                      description: _descriptionController.text,
+                    ),
+                  );
+                }
               },
               child: const Text('Salvar'),
             ),
@@ -60,5 +87,50 @@ class _EditTodoFormState extends State<EditTodoForm> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    widget.todoDetailsViewmodel.updateTodo.removeListener(_onUpdateTodo);
+    super.dispose();
+  }
+
+  void _onUpdateTodo() {
+    final command = widget.todoDetailsViewmodel.updateTodo;
+
+    if (command.running) {
+      showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            content: IntrinsicHeight(
+              child: Center(child: CircularProgressIndicator()),
+            ),
+          );
+        },
+      );
+    } else {
+      Navigator.of(context).pop();
+      if (command.completed) {
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Tarefa editada com sucesso!'),
+            backgroundColor: Colors.green,
+          ),
+        );
+      } else {
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Ocorreu um erro ao editar a Tarefa'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    }
   }
 }
